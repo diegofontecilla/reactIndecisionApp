eb3115335f3e4b88378b26eb5ad19cbab362220c
diego fontecilla <mail@diegofontecilla.com>
2018-12-12 15:59:30 +0000

    Adding state to VisibilityToggle

--- src/playground/build-it-visible.js
+++ src/playground/build-it-visible.js
@@ -1,26 +1,33 @@
-let visibility = false;
+class VisibilityToggle extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleToggleVisibility = this.handleToggleVisibility.bind(this);
+        this.state = {
+            visibility: false
+        };
+    }
+    handleToggleVisibility() {
+        this.setState((prevState) => {
+            return {
+                visibility: !prevState.visibility
+            };
+        });
+    }
+    render() {
+        return (
+            <div>
+                <h1>Visibility Toggle</h1>
+                <button onClick={this.handleToggleVisibility}>
+                    {this.state.visibility ? 'hide details' : 'show details'}
+                </button>
+                {this.state.visibility && (
+                    <div>
+                        <p>Hey, this are some details</p>
+                    </div>
+                )}
+            </div>
+        )
+    }
+}
 
-const toggleVisibility = () => {
-    visibility = !visibility;
-    render();
-};
-
-const render = () => {
-    const jsx = (
-        <div>
-            <h1>Visibility Toggle</h1>
-            <button onClick={toggleVisibility}>
-                {visibility ? 'hide details' : 'show details'}
-            </button>
-            {visibility && (
-                <div>
-                    <p>Hey, this are some details</p>
-                </div>
-            )}
-        </div>
-    );
-
-    ReactDOM.render(jsx, document.getElementById('app'));
-};
-
-render();
+ReactDOM.render(<VisibilityToggle />, document.getElementById('app'));
--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -11,7 +11,7 @@ class Counter extends React.Component {
     handleAddOne() {
         this.setState((prevState) => {
             return {
-            count: prevState.count + 1
+                count: prevState.count + 1
             };
         });
     }
@@ -42,35 +42,3 @@ class Counter extends React.Component {
 }
 
 ReactDOM.render(<Counter />, document.getElementById('app'));
-
-
-// let count = 0;
-// const addOne = () => {
-//     count++;
-//     renderCounterApp();
-// };
-// const minusOne = () => {
-//     count--;
-//     renderCounterApp();
-// };
-// const reset = () => {
-//     count = 0;
-//     renderCounterApp();
-// };
-
-// const appRoot = document.getElementById('app');
-
-// const renderCounterApp = () => {
-//     const templateTwo = (
-//         <div>
-//             <h1>Count: {count}</h1>
-//             <button onClick={addOne}>+1</button>
-//             <button onClick={minusOne}>-1</button>
-//             <button onClick={reset}>reset</button>
-//         </div>
-//     );
-
-//     ReactDOM.render(templateTwo, appRoot);
-// };
-
-// renderCounterApp();
