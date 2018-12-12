2be492880c612eef4fd2663fc140bc4666b0765c
diego fontecilla <mail@diegofontecilla.com>
2018-12-12 12:54:36 +0000

    Binding methods on counter-example.js with bind()

--- src/app.js
+++ src/app.js
@@ -40,8 +40,13 @@ class Action extends React.Component {
 }
 
 class Options extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleRemoveAll = this.handleRemoveAll.bind(this);
+    }
     handleRemoveAll() {
-        alert('handle remove all');
+        console.log(this.props.options);
+        // alert('handle remove     all');
     }
     render() {
         return (
